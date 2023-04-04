d23487627f8f5300e9e7bc9da6fdd32ede6efb12
Nicholas <97000198+ShortTimeNoSee@users.noreply.github.com>
2023-04-03 22:46:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const sliders = document.querySelectorAll(".slider");
 const calculateBtn = document.getElementById("calculate-btn");
 const ratingSpan = document.getElementById("rating");
+const tooltipPadding = 10; // adjust this value to change the spacing between the label and tooltip
 
 calculateBtn.addEventListener("click", () => {
 	let total = 0;
@@ -19,12 +20,23 @@ calculateBtn.addEventListener("click", () => {
 sliders.forEach((slider) => {
     const range = slider.querySelector(".range");
     const value = slider.querySelector(".value");
-    const naBtn = slider.querySelector(".na-btn"); // add this line
+    const label = slider.querySelector(".label"); // add this line
+    const tooltip = label.querySelector(".tooltip"); // add this line
+    const naBtn = slider.querySelector(".na-btn");
     value.textContent = range.value;
     range.addEventListener("input", () => {
         value.textContent = range.value;
     });
-    naBtn.addEventListener("click", () => { // add this block
-        naBtn.classList.toggle("selected");
+
+    label.addEventListener("mouseover", () => { // modify this event listener
+        const labelRect = label.getBoundingClientRect();
+        const tooltipRect = tooltip.getBoundingClientRect();
+        const distanceFromLeftEdge = labelRect.left;
+        let tooltipLeft = distanceFromLeftEdge + labelRect.width + tooltipPadding;
+        if (tooltipLeft + tooltipRect.width > window.innerWidth) {
+            tooltipLeft = window.innerWidth - tooltipRect.width;
+        }
+        tooltip.style.top = `${labelRect.top - tooltipRect.height}px`;
+        tooltip.style.left = `${tooltipLeft}px`;
     });
 });
