c506e2c9f37afc23c07c5dcaace23401d5861f46
Nicholas <97000198+ShortTimeNoSee@users.noreply.github.com>
2023-04-03 20:33:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const calculateBtn = document.getElementById("calculate-btn");
 const ratingSpan = document.getElementById("rating");
 
 calculateBtn.addEventListener("click", () => {
-  let total = 0;
-  sliders.forEach((slider) => {
-    const value = parseFloat(slider.querySelector(".value").textContent);
-    total += value;
-  });
-  const rating = (total / sliders.length).toFixed(1);
-  ratingSpan.textContent = rating;
+	let total = 0;
+	sliders.forEach((slider) => {
+		const value = parseFloat(slider.querySelector(".value").textContent);
+		total += value;
+	});
+	const rating = (total / sliders.length).toFixed(1);
+	ratingSpan.textContent = rating;
 });
 
 sliders.forEach((slider) => {
-  const range = slider.querySelector(".range");
-  const value = slider.querySelector(".value");
-  value.textContent = range.value;
-  range.addEventListener("input", () => {
-    value.textContent = range.value;
-  });
+	const range = slider.querySelector(".range");
+	const value = slider.querySelector(".value");
+	value.textContent = range.value;
+	range.addEventListener("input", () => {
+		value.textContent = range.value;
+	});
 });
