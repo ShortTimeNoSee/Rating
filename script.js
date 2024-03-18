7dbfdf310de73ee5ff9df3f7ba4b502d84de4380
Nicholas <97000198+ShortTimeNoSee@users.noreply.github.com>
2024-03-18 13:13:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const ratingSpan = document.getElementById("rating");
 
 calculateBtn.addEventListener("click", () => {
-	let total = 0;
-	let count = 0; // tracks the number of non-"N/A" sliders
-	sliders.forEach((slider) => {
-		const value = parseFloat(slider.querySelector(".value").textContent);
-		if (!slider.querySelector(".na-btn").classList.contains("selected")) {
-			total += value;
-			count++;
-		}
-	});
-	const rating = (count > 0 ? (total / count).toFixed(1) : "-");
-	ratingSpan.textContent = rating;
     let total = 0;
     let count = 0;
     sliders.forEach((slider) => {
