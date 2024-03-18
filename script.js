5b8a74e0ea9892762cc96b6d6c8f813e8edff1b4
Nicholas <97000198+ShortTimeNoSee@users.noreply.github.com>
2024-03-18 13:05:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,20 +5,14 @@ const ratingSpan = document.getElementById("rating");
 function decimalToFraction(value) {
   const fractions = [
     "0",
-    "π/30",
     "π/15",
-    "π/10",
-    "π/8",
-    "π/6",
+    "π/7.5",
     "π/5",
     "π/4",
-    "2π/9",
     "π/3",
     "5π/12",
     "7π/18",
-    "4π/9",
-    "5π/10",
-    "7π/15",
+    "2π/5",
     "π/2",
     "9π/18",
     "5π/9",
@@ -31,9 +25,15 @@ function decimalToFraction(value) {
     "3π/2",
     "17π/10",
     "19π/15",
-    "29π/30",
-    "π",
-    "31π/30"
+    "23π/12",
+    "25π/18",
+    "4π/3",
+    "27π/18",
+    "29π/15",
+    "31π/12",
+    "11π/6",
+    "37π/15",
+    "2π"
   ];
   const index = Math.round((value * 30) / (2 * Math.PI));
   return fractions[index];
